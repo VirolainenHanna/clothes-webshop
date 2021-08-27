6271de0b60be6091928a085be474f12bdc92add4
Hanna <60434409+VirolainenHanna@users.noreply.github.com>
2021-08-27 15:09:38 +0300

    Delete Products directory

--- Products/customers.sql
+++
@@ -1,5 +0,0 @@
-insert into customers(customer_id,name,email,date_joined) values('1','Ella Fizgerald','ella.f@email.com','20201005');
-insert into customers(customer_id,name,email,date_joined) values('2','Greta Jones','greta.j@email.com','20201005');
-insert into customers(customer_id,name,email,date_joined) values('3','Jenny Davis','jenny.d@email.com','20201005');
-insert into customers(customer_id,name,email,date_joined) values('4','Miley Button','miley.b@email.com','20201006');
-insert into customers(customer_id,name,email,date_joined) values('5','Elsi Kaarre','elsi.k@email.com','20201010');
