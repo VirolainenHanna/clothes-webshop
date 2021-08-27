cd570feac63532b978321989a626c4af1de26974
Hanna <60434409+VirolainenHanna@users.noreply.github.com>
2021-08-27 15:10:02 +0300

    Add files via upload

---
+++ Clothes-webshop/tables/order_products.sql
@@ -0,0 +1,6 @@
+insert into order_product(order_id,product_id,quantity) values('1','3','1');
+insert into order_product(order_id,product_id,quantity) values('1','13','1');
+insert into order_product(order_id,product_id,quantity) values('2','4','1');
+insert into order_product(order_id,product_id,quantity) values('2','14','1');
+insert into order_product(order_id,product_id,quantity) values('3','30','2');
+insert into order_product(order_id,product_id,quantity) values('4','28','1');
