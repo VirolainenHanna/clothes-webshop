ffd8859681498f3851b97429c4cf0558aba0a6d4
Hanna <60434409+VirolainenHanna@users.noreply.github.com>
2021-08-27 17:11:24 +0300

    Delete Clothes-webshop directory

--- Clothes-webshop/tables/orders.sql
+++
@@ -1,4 +0,0 @@
-insert into orders(order_id,customer_id,order_date) values('1','1','20201005');
-insert into orders(order_id,customer_id,order_date) values('2','2','20201005');
-insert into orders(order_id,customer_id,order_date) values('3','3','20201005');
-insert into orders(order_id,customer_id,order_date) values('4','1','20201505');
