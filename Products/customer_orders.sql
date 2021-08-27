ebb772172efc4ec02c74c711b38daf401c66f7a4
Hanna <60434409+VirolainenHanna@users.noreply.github.com>
2021-08-27 15:06:46 +0300

    Add files via upload

---
+++ Products/customer_orders.sql
@@ -0,0 +1,4 @@
+insert into customer_orders(order_id,order_date,customer_id,name) values('4','20201505','1','Ella Fizgerald');
+insert into customer_orders(order_id,order_date,customer_id,name) values('1','20201005','1','Ella Fizgerald');
+insert into customer_orders(order_id,order_date,customer_id,name) values('2','20201005','2','Greta Jones');
+insert into customer_orders(order_id,order_date,customer_id,name) values('3','20201005','3','Jenny Davis');
